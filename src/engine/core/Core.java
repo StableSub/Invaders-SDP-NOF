30b540743762ba49af686ef0317b378d7538c959
fkrdud1125 <fkrdud1125@naver.com>
2024-11-27 13:41:22 +0900

    Bug fix

--- src/engine/core/Core.java
+++ src/engine/core/Core.java
@@ -108,7 +108,7 @@ public final class Core {
 		Wallet wallet = db.loadWalletData(login.getID());
 		int returnCode = 1;
 		do {
-			MAX_LIVES = 1;
+			MAX_LIVES = 3+ wallet.getLives_lv()-1;
 			GameState gameState = new GameState(1, 0, BASE_SHIP, MAX_LIVES, 0, 0, 0, "", 0, 0, 0 ,0, 0);
 			Achievement achievement = db.loadAchData(login.getID());
 			AchievementManager achievementManager = new AchievementManager(achievement);
